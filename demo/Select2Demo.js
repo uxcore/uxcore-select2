1ca6a81c0e641a3a5275171c9ca80b0a9a38d9ac
光弘 <guanghong.wsj@alibaba-inc.com>
2015-10-09 20:05:30 +0800

    add ajax demo

--- demo/Select2Demo.js
+++ demo/Select2Demo.js
@@ -30,9 +30,32 @@ class Demo extends React.Component {
             value3: "",
             value4: "",
             options: [],
+            ajaxData: [],
             cities: cityData[provinceData[0]],
             secondCity:cityData[provinceData[0]][0]
         };
+    }
+
+    fetchData(value) {
+        let me = this;
+        $.ajax({
+            url: "http://suggest.taobao.com/sug",
+            dataType: "jsonp",
+            data: {q: value},
+            success: (data) => {
+                me.setState({
+                    ajaxData: data.result
+                });
+            },
+            fail: () => {
+                console.log("Fetch Data failed");
+            }
+        })
+    }
+
+    handleSearch(value) {
+        let me = this;
+        me.fetchData(value);
     }
 
     handleMailChange(value) {
@@ -62,11 +85,15 @@ class Demo extends React.Component {
         });
     }
     render() {
+        let me = this;
         let provinceOptions = provinceData.map(function(province) {
             return <Option key={province}>{province}</Option>;
         });
         let cityOptions = this.state.cities.map(function(city) {
             return <Option key={city}>{city}</Option>;
+        });
+        let ajaxOptions = me.state.ajaxData.map((d,index) => {
+            return <Option key={d[1]}>{d[0]}</Option>;
         });
         return (
             <div>
@@ -87,6 +114,10 @@ class Demo extends React.Component {
                 <p>多选:</p>
                 <Select multiple style={{width:400}} defaultValue={['a10', 'c12']} onChange={handleChange}>
                     {children}
+                </Select>
+                <p>多选提示，提示项根据 ajax 获得</p>
+                <Select multiple filterOption={false} style={{width: 400}} onSearch={me.handleSearch.bind(me)} onChange={handleChange}>
+                    {ajaxOptions}
                 </Select>
                 <p>标签:(标签的意义是，用户可以通过键盘自己输入值，而不局限于传入的选项)</p>
                 <Select style={{width: '100%'}} searchPlaceholder="标签模式" tags onChange={handleChange}>
