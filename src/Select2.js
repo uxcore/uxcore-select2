ef49009d3388e2154f5555aa5c76a05efb6fb68e
曹柯 <caoke.ck@alibaba-inc.com>
2015-10-20 22:26:48 +0800

    fix issue #6

--- src/Select2.js
+++ src/Select2.js
@@ -2,14 +2,30 @@ let RcSelect = require('rc-select');
 let assign = require('object-assign');
 
 class Select2 extends RcSelect {
-	constructor(props){
-		super(props);
-	}
+    constructor(props) {
+        super(props);
+    }
+    componentDidUpdate() {
+        var me = this;
+        var haveOpened = me.haveOpened;
+        me.haveOpened = false;
+        if (haveOpened) {
+            React.render(me.getDropdownElement(), (function() {
+                if (!me.dropdownContainer) {
+                    me.dropdownContainer = document.createElement('div');
+                    me.dropdownContainer.className = 'uxcore';
+                    document.body.appendChild(me.dropdownContainer);
+                }
+                return me.dropdownContainer;
+            })());
+        }
+        super.componentDidUpdate();
+        me.haveOpened = haveOpened;
+    }
 }
 Select2.displayName = 'Select2';
 Select2.defaultProps = assign({}, RcSelect.defaultProps, {
     prefixCls: "kuma-select2",
-	dropdownClassName: 'uxcore',
     optionLabelProp: "children"
 });
 
