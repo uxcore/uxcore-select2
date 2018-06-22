4213d1f1a912d7deecb6bc1018beaf8ca0c0959a
micate <micate@me.com>
2018-06-22 11:33:15 +0800

    feat: expose rc-select focus/blur api

--- src/Select2.jsx
+++ src/Select2.jsx
@@ -4,6 +4,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Select2 extends React.Component {
+  focus(...args) {
+    this.select.focus(...args);
+  }
+
+  blur(...args) {
+    this.select.blur(...args);
+  }
+
   render() {
     const me = this;
     const className = classnames(me.props.className, {
@@ -14,6 +22,9 @@ class Select2 extends React.Component {
     });
     return (
       <RcSelect
+        ref={(c) => {
+          this.select = c;
+        }}
         {...this.props}
         className={className}
         dropdownClassName={dropdownClassName}
